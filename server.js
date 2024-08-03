ed8d81e700cfa025fb0cd4177c6a94d7c6c9830e
knoxie87 <andrew.knox87@gmail.com>
2024-08-03 21:17:47 +1200

    binding HTTP server to  port

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const port = process.env.PORT || 4000;
 
 const init = async () => {
 
     const server = Hapi.server({
-        port: 3000,
+        port,
         host: 'localhost'
     });
 
