7ac5f7e96b851dba646ecedf2ca16b1a5e0f6889
knoxie87 <andrew.knox87@gmail.com>
2024-08-08 21:59:47 +1200

    Deleted test data

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const db = new sqlite3.Database("./test.db", sqlite3.OPEN_READWRITE, (err) => {i
 let sql;
 
 const createTable = () => {
-    // sql =`drop table if exists notes`
+    sql =`drop table if exists notes`
     sql = `CREATE TABLE notes (id INTEGER PRIMARY KEY, subject text, note text)`;
     db.run(sql, (err) => {if (err) return console.error(err.message);}); 
 
